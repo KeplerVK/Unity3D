3301b93ad1d85c662afa3f8373b9dcc2eb54a2a6
KeplerVK <1009637800@qq.com>
2019-10-18 19:08:22 +0800

    add week5

---
+++ week5/UFO/Assets/Scripts/Controller/Interface.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameStatus {
+    Play, Win, Lose
+}
+
+public enum SceneStatus {
+    Waiting, Shooting
+}
+
+public interface IUserInterface {
+    void SendUFO();
+    void DestroyUFO(GameObject ufo);
+}
+
+public interface IScore {
+    void AddScore();
+    void SubScore();
+    int GetScore();
+}
+
+public interface IQueryStatus {
+    GameStatus QueryGameStatus();
+    SceneStatus QuerySceneStatus();
+}
+
+public interface ISetStatus {
+    void SetGameStatus(GameStatus gameStatus);
+    void SetSceneStatus(SceneStatus scenceStatus);
+}
+
+public interface IActionController {
+    int GetRound();
+    int GetUFONum();
+    void SendUFO(List<GameObject> usingUFOs);
+    void DestroyUFO(GameObject UFO);
+    void SceneUpdate();
+}
